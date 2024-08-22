4da2a59aa8d7b418874eee757e754dac32434200
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-08-22 14:52:17 +0300

    initial commit

---
+++ pac.ps1
@@ -0,0 +1,10 @@
+dotnet publish -c Release  ..\AinAlAtaaFoundation\AinAlAtaaFoundation\ -r win-x64 -o .\publish
+
+$version = "0.1.20"
+$pack_id = "AinAlAtaaFoundation"
+$main_exe = "AinAlAtaaFoundation.exe"
+$icon_path = ".\ain-al-ataa-foundation.ico"
+$splash_image = ".\ain-al-ataa.jpg"
+$framework = "net8.0-x64-desktop"
+$pack_title = "Ain Al-Ataa Foundation"
+vpk pack -u $pack_id -v $version -p .\publish -e $main_exe --icon $icon_path --splashImage $splash_image --framework $framework --packTitle $pack_title
