bd47d6594625e1ef2406e35b26fe5e32e0be1a5f
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-08-27 16:01:35 +0300

    Create new release

--- pac.ps1
+++ pac.ps1
@@ -1,6 +1,6 @@
 dotnet publish -c Release  ..\AinAlAtaaFoundation\AinAlAtaaFoundation\ -r win-x64 -o .\publish
 
-$version = "0.1.28"
+$version = "0.1.30"
 $pack_id = "AinAlAtaaFoundation"
 $main_exe = "AinAlAtaaFoundation.exe"
 $icon_path = ".\ain-al-ataa-foundation.ico"
@@ -8,9 +8,3 @@ $splash_image = ".\ain-al-ataa.jpg"
 $framework = "net8.0-x64-desktop"
 $pack_title = "Ain Al-Ataa Foundation"
 vpk pack -u $pack_id -v $version -p .\publish -e $main_exe --icon $icon_path --splashImage $splash_image --framework $framework --packTitle $pack_title
-
-git add .
-git commit -m "Create new release $version"
-git push
-git tag $version
-git push origin tag $version
