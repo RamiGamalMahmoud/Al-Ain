2d91a0e24732d1e7c60fab61baed6955ddd5be02
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-09-16 22:44:25 +0300

    Create new release 0.3.8

--- pac.ps1
+++ pac.ps1
@@ -1,6 +1,6 @@
 dotnet publish -c Release  ..\AinAlAtaaFoundation\AinAlAtaaFoundation\ -r win-x64 -o .\publish
 
-$version = "0.3.7"
+$version = "0.3.8"
 $pack_id = "AinAlAtaaFoundation"
 $main_exe = "AinAlAtaaFoundation.exe"
 $icon_path = ".\ain-al-ataa-foundation.ico"
